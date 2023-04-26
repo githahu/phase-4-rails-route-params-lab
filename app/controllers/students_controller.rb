dfdde0f53896c97f966fcc123af7305cbacece31
Edwin Mwangi Njoroge <edwin.njoroge@student.moringaschool.com>
2023-04-26 15:44:47 +0300

    assignment done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,17 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  def show
+    student = Student.find(params[:id])
+    render json: student 
+  end
+  def index
+   
+      @students = Student.where("first_name LIKE :query OR last_name LIKE :query", query: "%#{params[:name]}%")
+  
 
+    render json: @students
+  end
+ 
+    
 end
